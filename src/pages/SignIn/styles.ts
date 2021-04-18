86b7a00b794cec1c7ebf8ce6f261fab06d32f08a
Pedro Zocatelli <pedro@zocatelli.dev>
2021-04-18 16:18:01 -0300

    added button and input components

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -2,8 +2,9 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
   justify-content: center;
+  align-items: center;
+  padding: 0 30px;
 `;
 
 export const Title = styled.Text`
